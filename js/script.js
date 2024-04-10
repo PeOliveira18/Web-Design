bf92e5684731609ba0b2b2475850b7bd7d07c3c2
Pedro Oliveira <oliveirasilvapedro2@gmail.com>
2024-04-10 13:23:32 -0300

    Web-Design

---
+++ js/script.js
@@ -0,0 +1,16 @@
+alert("Hello, World!")
+prompt("Digite seu nome")
+confirm("Voce deseja sair")
+
+var nome = prompt("Digite seu nome");
+var idade = 18
+
+console.log("Ola"+nome+", seja bem-vindo!")
+console.log(`Ola ${nome}, como voce esta`)
+
+
+var nome2 = prompt("Qual seu nome?")
+var idade2 = prompt("Digite sua idade: ")
+var profissao = prompt("Digite seu trabalho: ")
+
+console.log(`Ola ${nome2}, voce tem ${idade2} e trabalha como ${profissao}`)
