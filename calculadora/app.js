427c44b6279a8088ffcc067bd5a85891175ff8f4
Pedro Oliveira <oliveirasilvapedro2@gmail.com>
2024-04-11 11:48:51 -0300

    melhorando calculador

--- calculadora/app.js
+++ calculadora/app.js
@@ -16,8 +16,33 @@ function clicar(){
 function vezes(){
     var n1 = parseFloat(document.getElementById('numero1').value)
     var n2 = parseFloat(document.getElementById('numero2').value)
-    var result = document.getElementById('result')
+    var resultado2 = document.getElementById('resultado2')
 
     var multiplicar = n1*n2
-    result.innerText = `O resultadoi foi ${multiplicar}`
+    resultado2.innerText = `O resultado foi ${multiplicar}`
+}
+
+
+function subtracao(){
+    var n1 = parseFloat(document.getElementById('numero1').value)
+    var n2 = parseFloat(document.getElementById('numero2').value)
+    var resultado3 = document.getElementById('resultado3')
+
+    var subtrair = n1-n2
+    resultado3.innerText = `O resultado foi ${subtrair}`
+}
+
+
+function divisao(){
+    var n1 = parseFloat(document.getElementById('numero1').value)
+    var n2 = parseFloat(document.getElementById('numero2').value)
+    var resultado4 = document.getElementById('resultado4')
+
+    if (n2 ==0){
+        resultado4.innerText = "Nao e possivel dividir por zero"
+    }else{
+        var dividir = n1/n2
+    resultado4.innerText = `O resultado foi ${dividir.toFixed(2)}`
+    }
+    
 }
