3b688011e1d4e42763afcd1ef6e18d48da6c1c25
Pedro Oliveira <oliveirasilvapedro2@gmail.com>
2024-08-14 22:41:19 -0300

    Aula

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    const nome = "Pedro Oliveira";
+    
+    return ( 
+        <>
+            <h1>Ola,{nome}</h1>
+        </>
+    );
+}
+
+export default Header;
